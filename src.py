f2f9bcf552bc24b00179135d1b2250560b2cceb5
julianknodt <julianknodt@gmail.com>
2023-06-07 00:44:34 -0700

    Bug fixes

--- src.py
+++ src.py
@@ -2,6 +2,15 @@ import numpy as np
 from scipy.sparse import linalg
 from scipy.sparse import lil_matrix
 
+def adj(v):
+  i,j = v
+  return [
+    (i+1,j),
+    (i-1,j),
+    (i,j+1),
+    (i,j-1),
+  ]
+
 # Apply the Laplacian operator at a given index
 def lapl(source, idxs):
   u = idxs[:, 0]
@@ -58,40 +67,24 @@ def poisson_blend(
   clamp_u = lambda v: np.clip(v, 0, src.shape[0]-1)
   clamp_v = lambda v: np.clip(v, 0, src.shape[1]-1)
 
-  in_mask = mask[u,v] == 1
-  # computing A
-  m = mask[clamp_u(u+1),v] == 1
-  A[m & in_mask] = -1
-  m = mask[clamp_u(u-1),v] == 1
-  A[m & in_mask] = -1
-  m = mask[u,clamp_v(v+1)] == 1
-  A[m & in_mask] = -1
-  m = mask[u,clamp_v(v-1)] == 1
-  A[m & in_mask] = -1
+  pts = { tuple(x[0].tolist()): i for i,x in enumerate(np.split(idxs, N, axis=0)) }
+  for p,i in pts.items():
+    for a in adj(p):
+      if a not in pts: continue
+      j = pts[a]
+      A[i,j] = -1
+
   A = A.tocsr()
 
   # computing b
   b = np.zeros((N,C))
   b[range(N)] = lapl(src, idxs)
   locs = loc(mask, idxs)
-  on_edge = locs == ON_EDGE
-  # squeeze last
-  sq_l = lambda v: v[..., None]
-  b = np.where(
-    sq_l(on_edge & (mask[clamp_u(u+1),v] == 0)),
-    dst[clamp_u(u+1),v],
-    b,
-  )
-  b = np.where(
-    sq_l(on_edge & (mask[np.maximum(u-1,0),v] == 0)),
-    dst[np.minimum(u+1,mask.shape[0]-1),v],
-    b,
-  )
-  b = np.where(
-    sq_l(on_edge & (mask[u,np.minimum(v+1,mask.shape[1]-1)] == 0)),
-    dst[u,np.minimum(v+1,mask.shape[1]-1)],
-    b,
-  )
+  for pt,i in pts.items():
+    if locs[i] != ON_EDGE: continue
+    for a in adj(pt):
+      if mask[a[0], a[1]] == 0:
+        b[i] += dst[a[0], a[1]]
 
   # done computing b
   x = np.stack([
@@ -103,10 +96,17 @@ def poisson_blend(
   composite[u,v] = x
   return composite
 
-N = 128
-out = poisson_blend(
-  src=np.random.randn(N,N,3),
-  dst=np.random.randn(N,N,3),
+def main():
+  import os
+  import cv2
+  src = cv2.imread("tmp/source.png")/255.
+  dst = cv2.imread("tmp/target.png")/255.
+  mask = cv2.imread("tmp/mask.png")/255.
+  mask[mask != 1] = 0
+  mask = mask[...,2]
+  out = poisson_blend(src=src, dst=dst, mask=mask)
+  print(out.min(), out.max())
+  cv2.imwrite("out.png", out * 255.)
 
-  mask=(np.random.rand(N,N) - 0.4).round()
-)
+if __name__ == "__main__": main()
+
